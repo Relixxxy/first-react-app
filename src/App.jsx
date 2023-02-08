06357f01e3934814f2a78536edbd2f9a0e46ed6d
Relixxxy <vut1111111@gmail.com>
2023-02-08 23:06:41 +0200

    Router Dom added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+import Navbar from "components/UI/Navbar/Navbar";
 import Posts from "components/pages/Posts";
+import About from "components/pages/About";
+import Error from "components/pages/Error";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 
 const App = () => {
-  return <Posts />;
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route element={<Posts />} path="/posts" />
+        <Route element={<About />} path="/about" />
+        <Route element={<Error />} path="/error" />
+        <Route element={<Navigate to="/error" />} path="*" />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
---
+++ src/components/pages/About.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1>Hello, how about some shish?</h1>
+    </div>
+  );
+};
+
+export default About;
